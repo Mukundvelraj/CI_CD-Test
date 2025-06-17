d086196e1b518604a7e0fb4bb0f0d2e1d96093c5
“Mukund” <mukundvelraj15@gmail.com>
2025-06-17 14:22:55 +0530

    Pipeline Commit done

---
+++ Pipeline_1.py
@@ -0,0 +1,12 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+def test_launch():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    url = "https://github.com/"
+    driver.get(url)
+    driver.maximize_window()
+    assert driver.title == "GitHub"
